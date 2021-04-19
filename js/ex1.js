9565f657836c8c6aecde662bfde26a9df0cfac10
damy <ekamiss22@gmail.com>
2021-04-19 15:05:16 +0900

    javascript

--- js/ex1.js
+++ js/ex1.js
@@ -1,4 +1,38 @@
-window.onload = function(){
+// --ex4---
+window.onload = addEventListener("load", function(){
+    var section = window.document.querySelector("#ex4");
+    var container = section.querySelector(".container");
+    var boxes = section.querySelectorAll(".box");
+    var button = section.querySelector("input[value='click']");
+
+    button.onclick = function(){
+        var box = boxes[0];
+        
+        let boxStyle = window.getComputedStyle(box);
+        var left = boxStyle.getPropertyValue("left");
+        
+        console.log(left);
+    }
+});
+
+// --ex3---
+window.onload = addEventListener("load", function(){
+    var section = window.document.querySelector("#ex3");
+    var span = section.querySelector(".span");
+    var button = section.querySelector(".btn");
+
+    button.onclick = function(){
+        
+        window.setInterval(function(){
+            var count = parseInt(span.innerText);
+            count--;
+            span.innerText = count;
+        }, 500 )
+    }
+});
+
+// --ex2---
+window.onload = addEventListener("load", function(){
     var section = window.document.getElementById("ex2");
     var x = section.getElementsByClassName("x-input")[0];
     var y = section.getElementsByClassName("y-input")[0];
@@ -11,16 +45,17 @@ window.onload = function(){
         var add = parseInt(x.value)+parseInt(y.value);
         result.innerText=add;
     }
-}
+});
 
-// window.onload= function(){
+// --ex1---
+window.onload = addEventListener("load", function(){
     
-//     var section= window.document.getElementById("ex1");
+    var section= window.document.getElementById("ex1");
     
-//     var submitButtons= section.getElementsByClassName("submit-button");
-//     var submitButton = submitButtons[0];
+    var submitButtons= section.getElementsByClassName("submit-button");
+    var submitButton = submitButtons[0];
 
-//     submitButton.onclick = function(){
-//         console.log("test");
-//     }
-// }
+    submitButton.onclick = function(){
+        console.log("test");
+    }
+});
